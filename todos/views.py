427f38509643ca05e95e88daaab335b10a414c01
Aichi Chang <phoebexxxc@gmail.com>
2020-01-08 21:30:27 +0000

    detail todo DELETE function

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView # get the APIView class from DRF
 from rest_framework.response import Response # get the Response class from DRF
 from rest_framework.permissions import IsAuthenticatedOrReadOnly # our IsAuthenticated permission import, there are a few different options here
-from rest_framework.status import HTTP_201_CREATED, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
 
 from .models import Todo, Tag
 from .serializers import PopulatedTodoSerializer, TodoSerializer
@@ -25,7 +25,7 @@ class ListView(APIView):
         if todo.is_valid():
             todo.save()
             return Response(todo.data, status=HTTP_201_CREATED)
-        return Response(todo.errors, status=HTTP_422_UNPROCESSABLE_ENTITY)
+        return Response(todo.errors, status=HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE)
 
 
 class DetailView(APIView):
@@ -33,22 +33,29 @@ class DetailView(APIView):
     def get(self, _request, pk):
         todo = Todo.objects.get(pk=pk) # get a book by id (pk means primary key)
         serializer = TodoSerializer(todo)
-
         return Response(serializer.data)
 
     def put(self, request, pk):
+        # why we attach the user as owner again in edit? shoundt it be added already when create the todo?
         request.data['owner'] = request.user.id
         todo = Todo.objects.get(pk=pk)
         if todo.owner.id != request.user.id:
             return Response(status=HTTP_401_UNAUTHORIZED)
-        
+       
         updated_todo = TodoSerializer(data=request.data)
-        
+       
         if updated_todo.is_valid():
             updated_todo.save()
             return Response(updated_todo.data)
-        return Response(status=HTTP_422_UNPROCESSABLE_ENTITY)
-        
+        return Response(status=HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE)
+
+    
+    def delete(self, request, pk):
+        todo = Todo.objects.get(pk=pk)
+        if todo.owner.id != request.user.id:
+            return Response(status=HTTP_401_UNAUTHORIZED)
+        todo.delete()
+        return Response(status=HTTP_200_OK)
 
 
 
